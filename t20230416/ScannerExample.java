a2bbc28ec062ddcc82d7c6c77b20c564e0c88931
gyurikim-1202 <2december@naver.com>
2023-04-16 23:22:51 +0900

    java변수 범위, 출력, 저장

---
+++ t20230416/ScannerExample.java
@@ -0,0 +1,35 @@
+package t20230416;
+
+import java.util.Scanner;
+
+//키보드 입력 데이터를 변수에 저장
+public class ScannerExample {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("x 값 입력: ");
+		String strX = scanner.nextLine();
+		int x = Integer.parseInt(strX);
+		
+		System.out.print("y 값 입력: ");
+		String strY = scanner.nextLine();
+		int y = Integer.parseInt(strY);
+		
+		int result = x + y;
+		System.out.println("x + y: " + result);
+		System.out.println();
+		
+		while(true) {
+			System.out.print("입력 문자열: ");
+			String data = scanner.nextLine();
+			if(data.equals("q")) {
+				break;
+			}
+		System.out.println("출력 문자열: " + data);
+		System.out.println();
+		}
+		System.out.println("종료");
+	}
+
+}
