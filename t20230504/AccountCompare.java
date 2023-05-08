e16c339241d303bd0cd353b769b3b4f9cf260056
gyurikim-1202 <2december@naver.com>
2023-05-08 18:01:25 +0900

    java예금계좌조회프로그램

---
+++ t20230504/AccountCompare.java
@@ -0,0 +1,10 @@
+package t20230504;
+
+class AccountCompare {
+//어느 쪽 예금 잔액이 많은가
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- t20230504/CarTester3.java
+++ t20230504/CarTester3.java
@@ -2,14 +2,14 @@ package t20230504;
 
 import t20230502.Day;
 
-public class CarTester2 {
+public class CarTester3 {
 
 	public static void main(String[] args) {
 		Car car1 = new ExCar("W221", 1845, 1490, 5205, 90.0, 
 				new Day(2015, 12, 24));
-		
-		car1.putSpec();  //사양 표시
-		
+		car1.move(10, 10);
+		System.out.println("총 주행 거리: " + ((ExCar)car1).getTotalmileage());
+
 	}
 
 }
